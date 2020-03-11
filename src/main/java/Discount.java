e80c8004f0a03a80c99092b6eaae081dca9c55bc
unknown <billbrown20000@gmail.com>
2020-03-11 19:34:45 -0400

    first commit

---
+++ src/main/java/Discount.java
@@ -0,0 +1,23 @@
+public class Discount {
+    public static void main(String args[])
+    {
+        double  dis,amount,markedprice,s;
+
+        markedprice=1000;
+
+        dis=60;
+
+        System.out.println("markedprice= "+markedprice);
+
+        System.out.println("discount rate="+dis);
+
+        s=100-dis;
+
+        amount= (s*markedprice)/100;
+
+        System.out.println("amount after discount="+amount);
+
+    }
+
+}
+
